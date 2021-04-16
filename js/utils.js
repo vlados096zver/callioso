2c4ddaa2f60c9dbebeef7b3b3dbc34e7518a27ae
vlados096zver <81378975+vlados096zver@users.noreply.github.com>
2021-04-16 13:05:38 +0300

    up

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.chartColors = {
+	red: '#83b16a',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
